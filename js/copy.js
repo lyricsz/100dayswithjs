3ffa1670f41734d4e9f8d3f9b30d496718ad434f
lyricsz <152181901+lyricsz@users.noreply.github.com>
2025-01-08 23:13:36 +0100

    Add files via upload

--- js/copy.js
+++ js/copy.js
@@ -1,14 +1,6 @@
-let alert = 10 / 1
-let key = ''
 
 function copy(a = {key: "HELLO", id: "WOW"}){
     let {id, key} = a
-
-    console.log(id, key, alert)
-
-    console.log(typeof introduction) 
+    const wordToCopy = document.getElementById(id);
+    
 }
-
-new Promise(() => copy()).finally(() => {
-    alert("Works!!!")
-})
