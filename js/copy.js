c7a9eabdd6867de315f30fa8af3fffe83725216b
lyricsz <152181901+lyricsz@users.noreply.github.com>
2025-02-02 18:23:31 +0100

    Add files via upload

--- js/copy.js
+++ js/copy.js
@@ -1,6 +1,60 @@
+const message = document.getElementById("message");
 
-function copy(a = {key: "HELLO", id: "WOW"}){
-    let {id, key} = a
-    const wordToCopy = document.getElementById(id);
-    
+document.querySelectorAll("code").forEach((el, i) => {
+    const button = document.createElement("button");
+    button.setAttribute("class", "btn")
+    button.textContent = "<<COPY>>";
+    button.accessKey = "text" + i;
+    button.onclick = copy;
+    el.appendChild(button);
+});
+
+document.querySelectorAll("div.copy").forEach((el, index) => {
+    el.id = "text" + index;
+});
+
+async function copy(a){
+    console.log(a);
+    const key = a.target.accessKey
+    console.log(key)
+    const wordToCopy = document.getElementById(key);
+    await navigator.clipboard.writeText(wordToCopy.outerText) 
+    .then (onfulfilled = (message) => {
+        showSuccess("Copying...");
+    }).catch(err => {
+        showFailure(err);
+    }).finally((err) => {
+        setTimeout(done, 1000, err)
+    })
 }
+
+function showFailure(message){
+    pasteMessage(message);
+}
+
+function showSuccess(message){
+    pasteMessage(message);
+}
+
+function done(e){
+    if (!e){
+        e = "Done"
+    }
+    pasteMessage(e);
+}
+
+function pasteMessage(message){
+    document.getElementById("message").textContent = message;
+    document.getElementById("message").style.opacity = 10;
+}
+
+function reduce_opacity(){
+    if (message.style.opacity > 0){
+        message.style.opacity -= 0.1
+    } else {
+        message.style.opacity = 0
+    }
+    setTimeout(reduce_opacity, 10)
+}
+
+reduce_opacity()
